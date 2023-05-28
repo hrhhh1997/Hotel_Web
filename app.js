b5ce07fb78d28caa3c9787b41dfcac9bb6e0c954
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-28 09:21:58 +0000

    Perform mior changes

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.get("/", (req, res)=> {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/about-us", (req, res)=> {
+    res.sendFile(__dirname + "/public/pages/about-us.html");
+});
+
+app.get("/amenities", (req, res)=> {
+    res.sendFile(__dirname + "/public/pages/amenities.html");
+});
+
+app.get("/reservations", (req, res)=> {
+    res.sendFile(__dirname + "/public/pages/reservations.html");
+});
+
+app.get("/gallery", (req, res)=> {
+    res.sendFile(__dirname + "/public/pages/gallery.html");
+});
+
+app.get("/contact-us", (req, res)=> {
+    res.sendFile(__dirname + "/public/pages/contact-us.html");
+});
+
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("Server started.");
 })
